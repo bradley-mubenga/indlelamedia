b7eca1e60142f4963d3a5dad2e14d03dccca30c4
Bradley Mubenga <bradley@Bradleys-MacBook-Air.local>
2021-07-24 23:22:19 +0200

    Working On Full Page Js

---
+++ pages/_app.js
@@ -0,0 +1,10 @@
+///SCSS & CSS
+import '../assets/scss/home.scss';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/fullpage.js/dist/fullpage.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+//React Modules
+import React from 'react';
+import Head from 'next/head';
+
+export default function Index() {
+    return (
+        <div>
+            <Head>
+                <title>This Is The Home Page</title>
+            </Head>
+        </div>
+    )
+}
