7de96aab9a0f9564588bc0bb7286b72590bc1b3f
Bradley Mubenga <bradley@Bradleys-MacBook-Air.local>
2021-07-27 07:00:47 +0200

    Call To Action Fully Responsive

--- components/home.js/CallToAction.jsx
+++ components/home.js/CallToAction.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 //Components
 import { GreenButton } from '../buttons/buttons';
 
 export default function CallToAction() {
+    
+    //Responsive Device State For Extra Large Devices
+    const [ largeDevice, setContainer ] = useState(false);
+
+    useEffect(() => {
+        if (window.innerWidth >= 1200) {
+            setContainer(true)
+        }
+
+        else {
+            setContainer(false)
+        }
+    })
+
     return (
         <section id="callToActionSection" className="p-5">
-            <div className="container">
-                <div className="row text-center">
-                    <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
-                        <p className="lead callToActionText">We provide crucial data that inform companies on the best candidates to approach for CSI/SED initiatives.</p>
-                    </div>
+            <div className={largeDevice ? "container" : ""}>
+                <div className="container">
+                    <div className="row text-center">
+                        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                            <p className="lead callToActionText">We provide crucial data that inform companies on the best candidates to approach for CSI/SED initiatives.</p>
+                        </div>
 
-                    <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
-                        <GreenButton text={"How We Do It"} route={"/learn-more"}/>
+                        <div className="col-sm-12 col-md-12 col-lg-12 col-xl-12 text-center">
+                            <GreenButton text={"How We Do It"} route={"/learn-more"}/>
+                        </div>
                     </div>
                 </div>
             </div>
