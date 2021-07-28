4d2c5ed193e240b9ac87470b8effbae7bc47a49c
Bradley Mubenga <bradley@Bradleys-MacBook-Air.local>
2021-07-28 13:19:15 +0200

    Responsive Process Section Complete

--- components/home.js/TheProcess.jsx
+++ components/home.js/TheProcess.jsx
@@ -1,27 +1,32 @@
 //
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 
 export default function TheProcess() {
+
+
     return (
         <section className="py-5">
             <div className="container text-center">
                 <h1 className="processH1 pb-4">A Simple Process</h1>
 
                 <div className="row">
-                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3">
-                        <Image src="/1.Discovery.png" width="325px" height="325px" alt="Indlela Media Logo" className="img-fluid"/>
-                        <h3>DISCOVERY</h3>
+                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3 processIconBox1">
+                        <Image src="/1.Discovery.png" width="325px" height="325px" alt="Indlela Media Logo" className="img-fluid processIcon"/>
+                        <h3 className="processIconH1">DISCOVERY</h3>
+                        <p className="processIconText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta laboriosam voluptatem sed consequuntur. Exercitationem nulla doloribus possimus magnam!</p>
                     </div>
 
-                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3">
+                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3 processIconBox2">
                         <Image src="/2. Alignment.png" width="325px" height="325px" alt="Indlela Media Logo" className="img-fluid"/>
-                        <h3>ALIGNMENT</h3>
+                        <h3 className="processIconH1">ALIGNMENT</h3>
+                        <p className="processIconText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta laboriosam voluptatem sed consequuntur. Exercitationem nulla doloribus possimus magnam!</p>
                     </div>
 
-                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3">
+                    <div className="col-sm-12 col-md-12 col-lg-4 col-xl-4 py-3 processIconBox3">
                     <Image src="/3. Launch.png" width="325px" height="325px" alt="Indlela Media Logo" className="img-fluid"/>
-                    <h3>LAUNCH</h3>
+                    <h3 className="processIconH1">LAUNCH</h3>
+                    <p className="processIconText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates dicta laboriosam voluptatem sed consequuntur. Exercitationem nulla doloribus possimus magnam!</p>
                     </div>
                 </div>
             </div>
